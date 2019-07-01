6c8571c53fc32377cf60c4936c9e1ce1112c847e
DongOnee <dongwon5520@gmail.com>
2019-07-01 13:30:37 +0900

    ver1.1 Fix#3 search function

--- src/DLinkedList.c
+++ src/DLinkedList.c
@@ -67,14 +67,27 @@ void add(DLinkedList *dlinkedlist_ptr, Node *node)
 Node* search(DLinkedList *dlinkedlist_ptr, char* url)
 {
     Node* cur_node = dlinkedlist_ptr->header;
-    if (cur_node == NULL);
-    else while (strcmp(cur_node->url, url) != 0 && cur_node != NULL) cur_node = cur_node->next;
+    while (cur_node != NULL)
+    {
+        if (strcmp(cur_node->url, url) != 0) cur_node = cur_node->next;
+        else break;
+    }
 
     if (cur_node == NULL) return NULL;
     else if (strcmp(cur_node->url, url) == 0)
     {
-        cur_node->prev->next = cur_node->next;
-        cur_node->next->prev - cur_node->prev;
+        if(cur_node == dlinkedlist_ptr->header)
+        {
+            dlinkedlist_ptr->header = cur_node->next;
+            dlinkedlist_ptr->header->prev = NULL;
+        } 
+        else if (cur_node == dlinkedlist_ptr->tail) return cur_node;
+        else
+        {
+            cur_node->next->prev = cur_node->prev;
+            cur_node->prev->next = cur_node->next;
+        }
+
         dlinkedlist_ptr->tail->next = cur_node;
         cur_node->prev = dlinkedlist_ptr->tail;
         dlinkedlist_ptr->tail = cur_node;
